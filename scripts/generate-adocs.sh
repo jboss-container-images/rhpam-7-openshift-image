3df4c182cee2fc6ec0fd024a1bd94afec335ad80
spolti <fspolti@redhat.com>
2021-04-12 19:13:10 -0300

    [KIECLOUD-449] - Provide a Make command to generate adocs for RHPAM and RHDM templates
    Signed-off-by: spolti <fspolti@redhat.com>

---
+++ scripts/generate-adocs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+JBOSS_KIE_MODULES_REPO="https://github.com/jboss-container-images/jboss-kie-modules.git"
+TOOLS_BASE_DIR="tools/gen-template-doc"
+BRANCH="${1:-master}"
+CURRENT_DIR=$(pwd)
+
+rm -rf /tmp/jboss-kie-modules
+cd /tmp && git clone ${JBOSS_KIE_MODULES_REPO} && cd jboss-kie-modules
+
+git checkout origin/${BRANCH}
+
+cd "${TOOLS_BASE_DIR}"
+python gen_template_docs.py  --local-fs ${CURRENT_DIR} --rhpam --rhpam-docs-final-location=${CURRENT_DIR}/templates/docs/ --copy-docs
