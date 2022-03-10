c89ad7ef6453f819f80ee2910e1cfeb5fd48bedc
Spolti <fspolti@redhat.com>
2022-03-10 12:58:56 -0300

    [KIECLOUD-628] - Update rhdm quickstarts references on operator (#611)
    Signed-off-by: spolti <fspolti@redhat.com>

--- scripts/generate-adocs.sh
+++ scripts/generate-adocs.sh
@@ -3,7 +3,7 @@
 JBOSS_KIE_MODULES_REPO="https://github.com/jboss-container-images/jboss-kie-modules.git"
 TOOLS_BASE_DIR="tools/gen-template-doc"
 BRANCH="${1:-main}"
-CURRENT_DIR=$(pwd)
+CURRENT_DIR="$(pwd)/templates"
 
 rm -rf /tmp/jboss-kie-modules
 cd /tmp && git clone ${JBOSS_KIE_MODULES_REPO} && cd jboss-kie-modules
@@ -11,4 +11,4 @@ cd /tmp && git clone ${JBOSS_KIE_MODULES_REPO} && cd jboss-kie-modules
 git checkout origin/${BRANCH}
 
 cd "${TOOLS_BASE_DIR}"
-python gen_template_docs.py  --local-fs ${CURRENT_DIR} --rhpam --rhpam-docs-final-location=${CURRENT_DIR}/templates/docs/ --copy-docs
+python gen_template_docs.py  --local-fs ${CURRENT_DIR} --rhpam --rhpam-docs-final-location=${CURRENT_DIR}/docs/ --copy-docs
